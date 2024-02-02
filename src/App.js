b116bde957380f056635e22a89b870999f875c9c
kachhadiyajanvi <janvikachhadiya1@gmail.com>
2024-02-02 15:02:45 +0530

    demo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Agecalculater from './agecalculater';
+
+function App() {
+  return (
+    <div className="App">
+     <Agecalculater></Agecalculater>
+    </div>
+  );
+}
+
+export default App;
